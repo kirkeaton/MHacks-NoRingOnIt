54c0ede99b8e445c3de2bc389a08aef71d713553
Kirk Eaton <contact@kirkeaton.ca>
2014-01-18 16:05:48 -0500

    added calender event

---
+++ src/com/example/mhacks_noringonit/CalEvent.java
@@ -0,0 +1,42 @@
+package com.example.mhacks_noringonit;
+
+public class CalEvent {
+
+	private String title;
+	private long start;
+	private long end;
+
+	public CalEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CalEvent(String title, long start, long end) {
+		this.title = title;
+		this.start = start;
+		this.end = end;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public long getStartTime() {
+		return this.start;
+	}
+
+	public void setStartTime(long start) {
+		this.start = start;
+	}
+
+	public long getEndTime() {
+		return this.end;
+	}
+
+	public void setEndTime(long end) {
+		this.end = end;
+	}
+}
