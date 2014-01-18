7e1f800e9494cd0a8b57aca5472f73cfdd66199c
Kirk Eaton <contact@kirkeaton.ca>
2014-01-18 18:02:54 -0500

    fixed my little error. why didn't I check for NULL?

--- src/com/example/mhacks_noringonit/MainActivity.java
+++ src/com/example/mhacks_noringonit/MainActivity.java
@@ -1,53 +1,30 @@
 package com.example.mhacks_noringonit;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
 import android.provider.CalendarContract.Instances;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
+
+
+
 // Testing the commenting
 
 public class MainActivity extends Activity {
 
-	@SuppressLint("InlinedApi")
 	public void addEvent(View view) {
-		Log.d("third", "third");
 		Intent intent = new Intent(Intent.ACTION_INSERT);
 		intent.setType("vnd.android.cursor.item/event");
-		intent.putExtra(Events.TITLE, "tester Android");
-		intent.putExtra(Events.EVENT_LOCATION, "Home suit home");
-		intent.putExtra(Events.DESCRIPTION, "Download Examples");
-
-		Log.d("fourth", "fourth");
-
-		GregorianCalendar calDate = new GregorianCalendar(2014, 0, 19);
-		intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-				calDate.getTimeInMillis());
-		intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-				calDate.getTimeInMillis());
-
-		intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false);
-
-		intent.putExtra(Events.RRULE,
-				"FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
-
-		intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
-		intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
-
-		Log.d("fifth", "fifth");
 		startActivity(intent);
 	}
 
@@ -70,37 +47,35 @@ public class MainActivity extends Activity {
 		String[] projection = new String[] { Events._ID, Events.TITLE,
 				Events.DTSTART, Events.DTEND, Events.DURATION };
 
-
-		
-		Cursor cursor = Instances.query(getContentResolver(), projection,
-				begin.getTimeInMillis(), end.getTimeInMillis());
-		
+		Cursor cursor = 
+				Instances.query(getContentResolver(), projection, begin.getTimeInMillis(), end.getTimeInMillis());
+				
 		cursor.moveToFirst();
+		if(cursor.getCount() > 0)
+		{
+			do {
+				CalEvent event = new CalEvent();
+				event.setTitle(cursor.getString(cursor.getColumnIndex("title")));
+				event.setStartTime(cursor.getColumnIndex("dtstart"));
 
-		do {
-			CalEvent event = new CalEvent();
-			event.setTitle(cursor.getString(cursor.getColumnIndex("title")));
-			event.setStartTime(cursor.getColumnIndex("dtstart"));
 
-
-			if (TextUtils.isEmpty(cursor.getString(cursor.getColumnIndex("duration")))) {
-				event.setEndTime(cursor.getColumnIndex("dtend"));
-				event.setIsDuration(false);
-			} else {
-				event.setEndTime(cursor.getColumnIndex("duration"));
-				event.setIsDuration(true);
-			}
-			events.add(event);
-		} while (cursor.moveToNext());
-		return events;
+				if (TextUtils.isEmpty(cursor.getString(cursor.getColumnIndex("duration")))) {
+					event.setEndTime(cursor.getColumnIndex("dtend"));
+					event.setIsDuration(false);
+				} else {
+					event.setEndTime(cursor.getColumnIndex("duration"));
+					event.setIsDuration(true);
+				}
+				events.add(event);
+			} while (cursor.moveToNext());
+		}
+		return events;		
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		/* Creates audio manager object */ 
-		AudioManager audioManager = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
 	}
 	
 		/* Turns Ringer to normal mode */ 
