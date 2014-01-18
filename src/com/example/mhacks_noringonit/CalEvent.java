b6880adb6b75db91897f53f23fa3f56e426dede0
Kirk Eaton <contact@kirkeaton.ca>
2014-01-18 16:28:06 -0500

    updated MainActivity.java to return a list of events and changed CalEvent.java

--- src/com/example/mhacks_noringonit/CalEvent.java
+++ src/com/example/mhacks_noringonit/CalEvent.java
@@ -5,15 +5,17 @@ public class CalEvent {
 	private String title;
 	private long start;
 	private long end;
+	private Boolean isDuration; // optional
 
 	public CalEvent() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public CalEvent(String title, long start, long end) {
+	public CalEvent(String title, long start, long end, Boolean duration) {
 		this.title = title;
 		this.start = start;
 		this.end = end;
+		this.isDuration = duration;
 	}
 	
 	public CalEvent(CalEvent ce)
@@ -21,6 +23,7 @@ public class CalEvent {
 		this.title = ce.title;
 		this.start = ce.start;
 		this.end = ce.end;
+		this.isDuration = ce.isDuration;
 	}
 
 	public String getTitle() {
@@ -45,5 +48,15 @@ public class CalEvent {
 
 	public void setEndTime(long end) {
 		this.end = end;
+	}
+	
+	public Boolean getIsDuration()
+	{
+		return this.isDuration;
+	}
+	
+	public void setIsDuration(Boolean isDuration)
+	{
+		this.isDuration = isDuration;
 	}
 }
--- src/com/example/mhacks_noringonit/MainActivity.java
+++ src/com/example/mhacks_noringonit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mhacks_noringonit;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -19,91 +20,80 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	private static final String DEBUG_TAG = "MyActivity";
-	public static final String[] INSTANCE_PROJECT = new String[] {  
-        Instances.EVENT_ID, // 0  
-        Instances.BEGIN, // 1  
-        Instances.TITLE, // 2  
-	};
-	
-	private static final int PROJECTION_ID_INDEX = 0;
-	private static final int PROJECTION_BEGIN_INDEX = 1;
-	private static final int PROJECTION_TITLE_INDEX = 2;
-	
 	@SuppressLint("InlinedApi")
-	public void addEvent(Activity ac)
-	{
+	public void addEvent(Activity ac) {
 		Log.d("third", "third");
-		Intent intent = new Intent(Intent.ACTION_INSERT);  
-        intent.setType("vnd.android.cursor.item/event");  
-        intent.putExtra(Events.TITLE, "tester Android");  
-        intent.putExtra(Events.EVENT_LOCATION, "Home suit home");  
-        intent.putExtra(Events.DESCRIPTION, "Download Examples");  
-        
-        Log.d("fourth", "fourth");
-   
-        GregorianCalendar calDate = new GregorianCalendar(2014, 0, 19);  
-        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,  
-                calDate.getTimeInMillis());  
-        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,  
-                calDate.getTimeInMillis());  
-   
-        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false);  
-   
-        intent.putExtra(Events.RRULE,  
-                "FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");  
-   
-        intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);  
-        intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);  
-   
-        Log.d("fifth", "fifth");
-        ac.startActivity(intent);  
+		Intent intent = new Intent(Intent.ACTION_INSERT);
+		intent.setType("vnd.android.cursor.item/event");
+		intent.putExtra(Events.TITLE, "tester Android");
+		intent.putExtra(Events.EVENT_LOCATION, "Home suit home");
+		intent.putExtra(Events.DESCRIPTION, "Download Examples");
+
+		Log.d("fourth", "fourth");
+
+		GregorianCalendar calDate = new GregorianCalendar(2014, 0, 19);
+		intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+				calDate.getTimeInMillis());
+		intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+				calDate.getTimeInMillis());
+
+		intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false);
+
+		intent.putExtra(Events.RRULE,
+				"FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
+
+		intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
+		intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
+
+		Log.d("fifth", "fifth");
+		ac.startActivity(intent);
 	}
-	
+
 	@SuppressLint("NewApi")
-	public void getEvents(Activity ac)
-	{
-		int year, month, date;
+	public List<CalEvent> getEvents(Activity ac) {
+		List<CalEvent> events = null;
 		
+		int year, month, date;
+
 		Calendar begin = Calendar.getInstance();
 		Calendar end = Calendar.getInstance();
-		
+
 		year = Calendar.getInstance().get(Calendar.YEAR);
 		month = Calendar.getInstance().get(Calendar.MONTH);
 		date = Calendar.getInstance().get(Calendar.DATE);
-		
+
 		begin.set(year, month, date, 0, 0);
 		end.set(year, month, date, 23, 59);
-		
-		String[] projection = new String[] {
-				Events._ID,
-				Events.TITLE,
-				Events.DTSTART,
-				Events.DTEND,
-				Events.DURATION};		
 
-		Cursor cursor = 
-				Instances.query(getContentResolver(), projection, begin.getTimeInMillis(), end.getTimeInMillis());
-		
+		String[] projection = new String[] { Events._ID, Events.TITLE,
+				Events.DTSTART, Events.DTEND, Events.DURATION };
+
+		Cursor cursor = Instances.query(getContentResolver(), projection,
+				begin.getTimeInMillis(), end.getTimeInMillis());
+
 		cursor.moveToFirst();
 
 		do {
-			Log.d(cursor.getString(cursor.getColumnIndex("title")),  cursor.getString(cursor.getColumnIndex("title")));
-			Log.d(cursor.getString(cursor.getColumnIndex("dtstart")),cursor.getString(cursor.getColumnIndex("dtstart")));
+			CalEvent event = new CalEvent();
+			event.setTitle(cursor.getString(cursor.getColumnIndex("title")));
+			event.setStartTime(cursor.getColumnIndex("dtstart"));
 
-			if(TextUtils.isEmpty(cursor.getString(cursor.getColumnIndex("duration"))))
-			{
-				Log.d(cursor.getString(cursor.getColumnIndex("dtend")),cursor.getString(cursor.getColumnIndex("dtend")));
-			} else
-			{
-				Log.d(cursor.getString(cursor.getColumnIndex("duration")),cursor.getString(cursor.getColumnIndex("duration")));
+
+			if (TextUtils.isEmpty(cursor.getString(cursor.getColumnIndex("duration")))) {
+				event.setEndTime(cursor.getColumnIndex("dtend"));
+				event.setIsDuration(false);
+			} else {
+				event.setEndTime(cursor.getColumnIndex("duration"));
+				event.setIsDuration(true);
 			}
+			events.add(event);
 		} while (cursor.moveToNext());
-	}	
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }    
+		return events;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 }
